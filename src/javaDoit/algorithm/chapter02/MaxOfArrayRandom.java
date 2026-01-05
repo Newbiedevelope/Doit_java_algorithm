2d031f77a2c7db674ea2a80d508b90701c85bb46
Newbiedevelope <rbtjr20313@gmail.com>
2026-01-05 18:53:39 +0900

    배열 속 최댓값 찾기 및 난수 생성

---
+++ src/javaDoit/algorithm/chapter02/MaxOfArrayRandom.java
@@ -0,0 +1,33 @@
+package javaDoit.algorithm.chapter02;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class MaxOfArrayRandom {
+    static int maxOf(int[] a){
+        int max = a[0];
+        for (int i=1; i<a.length; i++)
+            if (a[i] > max)
+                max = a[i];
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.println("키의 최댓값을 구합니다.");
+        System.out.print("사람 수 : ");
+        int num = stdIn.nextInt();
+
+        int[] height = new int[num];
+
+        System.out.println("킷값은 아래와 같습니다.");
+        for (int i=0; i<height.length; i++){
+            height[i] = 100 + random.nextInt(90);
+            System.out.println("height[" + i + "] : " + height[i]);
+        }
+
+        System.out.println("최댓값은 " + maxOf(height) + "입니다.");
+    }
+}
